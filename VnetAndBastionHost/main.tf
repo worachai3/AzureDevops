f37b3f83deffa2ff7197e266ca4bf9b9fbdd908e
benzWRC <worachai3@live.com>
2024-03-18 01:05:04 +0700

    Vnet And Bastion

---
+++ VnetAndBastionHost/main.tf
@@ -0,0 +1,65 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.96.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "vnet_rg" {
+  name = var.resourcegroup_name
+  location = var.location
+  tags = var.tags
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name = var.vnet_name
+  address_space = var.vnet_address_space
+  location = var.location
+  resource_group_name = var.resourcegroup_name
+  tags = var.tags
+
+  depends_on = [ azurerm_resource_group.vnet_rg ]
+}
+
+resource "azurerm_subnet" "subnet" {
+  for_each = var.subnets
+  resource_group_name = var.resourcegroup_name
+  virtual_network_name = var.vnet_name
+  name = each.value["name"]
+  address_prefixes = each.value["address_prefixes"]
+
+  depends_on = [ azurerm_resource_group.vnet_rg, azurerm_virtual_network.vnet ]
+}
+
+# Bastion Host doesn't need public ip this is for example only
+resource "azurerm_public_ip" "bastion_pubip" {
+  name = "${var.bastionhost_name}PubIP"
+  location = var.location
+  resource_group_name = var.resourcegroup_name
+  allocation_method = "Static"
+  sku = "Standard"
+  tags = var.tags
+
+  depends_on = [ azurerm_resource_group.vnet_rg ]
+}
+
+resource "azurerm_bastion_host" "bastion" {
+  name = var.bastionhost_name
+  location = var.location
+  resource_group_name = var.resourcegroup_name
+  tags = var.tags
+
+  ip_configuration {
+    name = "bastion_config"
+    subnet_id = azurerm_subnet.subnet["bastion_subnet"].id
+    public_ip_address_id = azurerm_public_ip.bastion_pubip.id
+  }
+
+  depends_on = [ azurerm_resource_group.vnet_rg, azurerm_public_ip.bastion_pubip ]
+}
---
+++ VnetAndBastionHost/variables.tf
@@ -0,0 +1,60 @@
+variable "resourcegroup_name" {
+  type        = string
+  description = "The name of the resource group"
+  default     = "TFTesTRob01"
+}
+
+variable "location" {
+  type        = string
+  description = "The region for the deployment"
+  default     = "CentralUS"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Tags used for the deployment"
+  default = {
+    "Environment" = "Lab"
+    "Owner"       = "Worachai"
+  }
+}
+
+variable "vnet_name" {
+  type        = string
+  description = "The name of the vnet"
+  default     = "VNetTFTest"
+}
+
+variable "vnet_address_space" {
+  type        = list(any)
+  description = "The address space of the VNet"
+  default     = ["10.13.0.0/16"]
+}
+
+variable "subnets" {
+  type = map(any)
+  default = {
+    subnet_1 = {
+      name             = "subnet_1"
+      address_prefixes = ["10.13.1.0/24"]
+    }
+    subnet_2 = {
+      name             = "subnet_2"
+      address_prefixes = ["10.13.2.0/24"]
+    }
+    subnet_3 = {
+      name             = "subnet_3"
+      address_prefixes = ["10.13.3.0/24"]
+    }
+    bastion_subnet = {
+      name             = "AzureBastionSubnet"
+      address_prefixes = ["10.13.250.0/24"]
+    }
+  }
+}
+
+variable "bastionhost_name" {
+  type = string
+  description = "The name of bastion host"
+  default = "BastionHost"
+}
