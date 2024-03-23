74a61e87ab3a7b684e1c9d12eec5945cc3b3bc1b
benzWRC <worachai3@live.com>
2024-03-24 01:53:37 +0700

    Update main.tf

--- VnetAndBastionHost/main.tf
+++ VnetAndBastionHost/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "3.96.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name = "tfstateRG01"
+    storage_account_name = "tfstate01670954844"
+    container_name = "tfstate"
+    key = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
